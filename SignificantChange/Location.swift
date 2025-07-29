ecb021017076313b8a6f72107a9de23414a6e907
Christopher Trott <chris@twocentstudios.com>
2025-07-29 11:28:17 +0900

    Location struct

---
+++ SignificantChange/Location.swift
@@ -0,0 +1,52 @@
+import CoreLocation
+import Foundation
+import SharingGRDB
+
+@Table struct Location: Hashable, Identifiable {
+    let id: UUID
+    var latitude: Double
+    var longitude: Double
+    var altitude: Double?
+    var timestamp: Date
+    var horizontalAccuracy: Double?
+    var verticalAccuracy: Double?
+    var course: Double?
+    var speed: Double?
+
+    init(
+        id: UUID = UUID(),
+        latitude: Double,
+        longitude: Double,
+        altitude: Double? = nil,
+        timestamp: Date = Date(),
+        horizontalAccuracy: Double? = nil,
+        verticalAccuracy: Double? = nil,
+        course: Double? = nil,
+        speed: Double? = nil
+    ) {
+        self.id = id
+        self.latitude = latitude
+        self.longitude = longitude
+        self.altitude = altitude
+        self.timestamp = timestamp
+        self.horizontalAccuracy = horizontalAccuracy
+        self.verticalAccuracy = verticalAccuracy
+        self.course = course
+        self.speed = speed
+    }
+}
+
+extension Location {
+    init(from clLocation: CLLocation) {
+        self.init(
+            latitude: clLocation.coordinate.latitude,
+            longitude: clLocation.coordinate.longitude,
+            altitude: clLocation.altitude,
+            timestamp: clLocation.timestamp,
+            horizontalAccuracy: clLocation.horizontalAccuracy,
+            verticalAccuracy: clLocation.verticalAccuracy,
+            course: clLocation.course >= 0 ? clLocation.course : nil,
+            speed: clLocation.speed >= 0 ? clLocation.speed : nil
+        )
+    }
+}

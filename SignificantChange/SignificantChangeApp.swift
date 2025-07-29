12b4893f76e23d27c90a21301d98112c1d938e95
Christopher Trott <chris@twocentstudios.com>
2025-07-29 10:25:15 +0900

    app delegate setup

--- SignificantChange/SignificantChangeApp.swift
+++ SignificantChange/SignificantChangeApp.swift
@@ -2,6 +2,8 @@ import SwiftUI
 
 @main
 struct SignificantChangeApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()

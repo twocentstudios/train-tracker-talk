42e92d69d7b77ce971de5901a9bdd0c63dd34976
Christopher Trott <chris@twocentstudios.com>
2025-08-13 11:27:59 +0900

    Set up initial RailwayTracker

--- SessionViewer/RailwayTracker.swift
+++ SessionViewer/RailwayTracker.swift
@@ -1,14 +1,50 @@
+import Foundation
 import SharingGRDB
+
+enum FocusStationPhase: Equatable, Codable, CustomDebugStringConvertible {
+    case upcoming
+    case approaching
+    case visiting
+
+    var title: LocalizedStringResource {
+        switch self {
+        case .upcoming: .init(stringLiteral: "focusStationPhase.upcoming")
+        case .approaching: .init(stringLiteral: "focusStationPhase.approaching")
+        case .visiting: .init(stringLiteral: "focusStationPhase.visiting")
+        }
+    }
+
+    var debugDescription: String {
+        switch self {
+        case .upcoming: .init(stringLiteral: "Next")
+        case .approaching: .init(stringLiteral: "Soon")
+        case .visiting: .init(stringLiteral: "Now")
+        }
+    }
+}
+
+struct RailwayTrackerCandidate: Equatable {
+    var railway: Railway
+    var railwayDestinationStation: Station?
+    var railwayDirection: RailDirection?
+    var focusStation: Station?
+    var focusStationPhase: FocusStationPhase?
+    var laterStation: Station?
+    var laterLaterStation: Station?
+}
 
 struct RailwayTrackerResult {
     let location: Location
-    let value: Int
+    var instantaneousRailwayCoordinateScores: [Railway.ID: Double]
+    var instantaneousRailwayCoordinates: [Railway.ID: Coordinate]
+    var candidates: [RailwayTrackerCandidate]
 }
 
 actor RailwayTracker {
     private let railwayDatabase: (any DatabaseReader)?
 
-    private var count: Int = 0
+    var railwayScores: [Railway.ID: Double] = [:]
+    var railwayAscendingScores: [Railway.ID: Double] = [:]
 
     init(railwayDatabase: (any DatabaseReader)?) {
         self.railwayDatabase = railwayDatabase
@@ -16,11 +52,16 @@ actor RailwayTracker {
 
     func process(_ input: Location) async -> RailwayTrackerResult {
         try? await Task.sleep(for: .seconds(0.01))
-        count += 1
-        return RailwayTrackerResult(location: input, value: count)
+        return RailwayTrackerResult(
+            location: input,
+            instantaneousRailwayCoordinateScores: [:],
+            instantaneousRailwayCoordinates: [:],
+            candidates: []
+        )
     }
 
     func reset() {
-        count = 0
+        railwayScores = [:]
+        railwayAscendingScores = [:]
     }
 }
